a9f0f237177f069fe7c8aaa03b3de75bbc5504c8
jorge luis <martinezmonajorgeluis@gmail.com>
2024-01-15 13:34:40 -0500

    This codeis dedicate to the class about the creation of string and also to the concatenation tool. the present commit pertain or correspond to the class number 9 of JavaScript since zero by Platzy

---
+++ clase-paso-por-valor-y-paso-por-referencia.js
@@ -0,0 +1,23 @@
+// Paso por valor
+let x = 1
+let y = 'Hola'
+let z = null
+
+let a = x
+let b = y
+let c = z
+console.log(x,y,z,a,b,c)
+
+a = 12
+b = 'Platzi'
+c = undefined
+
+// paso por refereancia
+let frutas= ['manzana']
+frutas.push('pera')
+console.log(frutas)
+
+let panes = ['ðŸ¥']
+let copiaDePanes = panes
+panes.push('ðŸ¥–')
+console.log(panes,copiaDePanes)
---
+++ clase-string.js
@@ -0,0 +1,56 @@
+// Creacion de string
+const primeraOpcion ='Comillas simples'
+const segundaOpcion ="Comillas simples"
+const terceraOpcion =`Comillas simples`
+
+// COncatenacion: opcion +
+const direccion ='Calle falsa 123'
+const ciudad ='Springfield'
+const direccionCompleta ='Mi direccion completa es '+ direccion + ciudad
+console.log(direccionCompleta)
+
+const direccionCompletaConEspacio ='Mi direcciÃ³n completa es '+ direccion + ' ' + ciudad
+console.log(direccionCompletaConEspacio)
+
+// COncatenacion: template literals
+const nombre ='Estefany'
+const pais ='ðŸ‡¨ðŸ‡´'
+const presentacion =`Hola, soy ${nombre} de ${pais}`
+console.log(presentacion)
+
+//COncatenacion: join()
+const primeraParte ='Me encanta'
+const segundaParte ='la gente de'
+const terceraParte ='Mexico'
+const pensamiento = [primeraParte,segundaParte,terceraParte]
+console.log(pensamiento.join(' '))
+
+//Concatenacion: concat()
+const hobbie1 ='correr'
+const hobbie2 ='leer'
+const hobbie3 ='estudiar'
+const hobbies ='Mis hobbies son: '.concat(hobbie1,', ',hobbie2,', ',hobbie3,'.')
+console.log(hobbies)
+
+
+const escapeAlternativo ="I'm Software Engineer"
+const escapeBarraInvertida ='I\'m Software Engineer'
+const escapeComillaInvertida =`I'm Software Engineer`
+
+const poema ='Las rosas son rojas,\n'+
+'Las violetas son azules,\n'+
+'Caracter inesperado,\n'+
+'En la lÃ­nea 86.'
+console.log(poema)
+
+const poema2 ='Las rosas son rojas,\n\
+Las violetas son azules,\n\
+Caracter inesperado,\n\
+En la lÃ­nea 86.'
+console.log(poema2)
+
+const poema3 =`Las rosas son rojas,
+Las violetas son azules,
+Caracter inesperado,
+En la lÃ­nea 86.`
+console.log(poema3)
