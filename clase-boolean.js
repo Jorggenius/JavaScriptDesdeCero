db29b159ddd8165010439e4d6c3244e10636a0d0
jorge luis <martinezmonajorgeluis@gmail.com>
2024-01-29 05:16:55 -0500

    This coder is about the booleans

---
+++ clase-boolean.js
@@ -0,0 +1,10 @@
+const isActive = true
+const hasPermisson = false
+const result = 5>3
+console.log(result)
+const name = 'Platzi'
+console.log(!!name)
+const value = 0
+const otherValue = -24
+const explicitBoolean = Boolean(otherValue)
+console.log(explicitBoolean)    
